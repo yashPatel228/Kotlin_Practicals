97d862506d0e69ab60cb22362cd7aafa919f2c30
student <studen@guni.com>
2026-06-23 14:43:08 +0530

    Initial commit

---
+++ src/p11.kt
@@ -0,0 +1,42 @@
+fun main(){
+    var i = 10
+    var j  :Int = 20
+    var k  :Float = 1.5f
+    var l  :Char = 'y'
+    var m  :String = "Yash"
+    var n  :Boolean = false
+    var o  :Double = 96.00
+    var p  :Long = 2254846
+    var q  :Short = 2
+    var r  : Byte = 127
+
+    val x = 100
+     j = 200
+
+    val score = 90
+            println ("your score is $score")
+    
+    val language = "kotlin"
+    println(language)
+
+    val Name = "John"
+    println("Hello " + Name)
+
+    val name = "Alice"
+    println("Length: ${name.length}")
+
+
+
+    println("i = "+i)
+    println("j = "+j)
+    println("k = "+k)
+    println("l = "+l)
+    println("m = "+m)
+    println("n = "+n)
+    println("o = "+o)
+    println("p = "+p)
+    println("q = "+q)
+    println("r = "+r)
+    println("x = "+x)
+
+}
---
+++ src/p12.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val i : Int = 100
+    val j : Double = 22.0
+    val s = "1000"
+    val a : Int = s.toInt()
+
+    println(i)
+    println(s)
+    println(a)
+    println(j)
+}
